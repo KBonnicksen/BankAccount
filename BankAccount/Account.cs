69a97730d73b911325392a74b483d6f7229adadd
KBonnicksen <kelseybon@gmail.com>
2019-07-31 16:46:50 -0700

    Added validation code

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -7,31 +7,53 @@ using System.Threading.Tasks;
 namespace BankAccount
 {
     /// <summary>
-    /// Represents an individual bank account
+    /// Represents a customers bank account
     /// </summary>
     public class Account
     {
-        public string AccountNumber { get; set; }
+        private string _accountNumber;
+
+        public string AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException
+                        ($"{nameof(AccountNumber)} cannot be null");
+                }
+                if (value.Contains("#"))
+                {
+                    throw new ArgumentException
+                        ($"{nameof(AccountNumber)} cannot contain # signs");
+                }
+                _accountNumber = value;
+            }
+        }
 
         public string Owner { get; set; }
 
         public double Balance { get; private set; }
 
         /// <summary>
-        /// Adds an amount to the current balance and returns
-        /// the updated balance
+        /// Adds an amount to the current balance
+        /// and returns the updated balance
         /// </summary>
-        /// <param name="amt">The amount being deposited</param>
+        /// <param name="amt">The amount to add</param>
+        /// <returns></returns>
         public double Deposit(double amt)
         {
+            if (amt < 0)
+                throw new ArgumentException($"{nameof(amt)} cannot be negative");
+
             Balance += amt;
             return Balance;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="amt">The amount being withdrawn</param>
         public double Withdraw(double amt)
         {
             Balance -= amt;
